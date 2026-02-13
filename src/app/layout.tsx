d26cd78fd752368ec8abeb974c00b68cdfe8a7fe
Sumit Singh <sumit.singh@edgenroots.net>
2026-02-13 14:56:50 +0530

    feat: Integrate Google Tag Manager and add `suppressHydrationWarning` to the HTML tag.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
   title: "Apply LoanInNeed.in",
   description: "Get Instant Loan Approval",
   icons: {
-    icon: "/logo1.png", 
+    icon: "/logo1.png",
   },
 };
 
@@ -29,26 +29,43 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NGZJ6DKC');`}
+        </Script>
         <Script
-  strategy="afterInteractive"
-  async
-  src="https://www.googletagmanager.com/gtag/js?id=AW-10980985072"
-/>
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
+          strategy="afterInteractive"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10980985072"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'AW-10980985072');
   `}
-</Script>
+        </Script>
 
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NGZJ6DKC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {/* ✅ Global Navbar */}
         <Navbar />
 
